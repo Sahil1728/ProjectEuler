46daf112a74afe1d14f06e3cbbec33c921460760
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-07 17:25:17 +0000

    Added python code for approach 2

---
+++ Problems 1 - 10/Problem 1/Approach2.py
@@ -0,0 +1,10 @@
+# n-> number till which we have to find the sum of multiples of 3 and 5
+n = int(input())
+# sum of multiples of 3
+threes = (n//3)*(2*3+(n//3-1)*3)//2
+# sum of multiples of 5
+fives = (n//5)*(2*5+(n//5-1)*5)//2
+# sum of multiples of 15
+fifteens = (n//15)*(2*15+(n//15-1)*15)//2
+# sum of multiples of 3 and 5
+print(threes+fives-fifteens)
